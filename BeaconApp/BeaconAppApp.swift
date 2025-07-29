89efa2fb3b9d349bc1fa65d27a0fdb665928737b
Ananya <ananyatheavenger@gmail.com>
2025-07-29 13:21:16 -0500

    Initial Commit

---
+++ BeaconApp/BeaconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconAppApp.swift
+//  BeaconApp
+//
+//  Created by Ananya Gogula on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BeaconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
