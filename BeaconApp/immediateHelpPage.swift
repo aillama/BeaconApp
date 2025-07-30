f320e2f0f74be1f2bf9bc57bc8d41be83ce79d9f
Shruti Shivakumar <shivakumarshruti1@gmail.com>
2025-07-30 14:11:33 -0400

    Changed background gradient.

--- BeaconApp/immediateHelpPage.swift
+++ BeaconApp/immediateHelpPage.swift
@@ -82,7 +82,7 @@ struct immediateHelpPage: View {
                 
             }
             
-            RadialGradient(colors: [Color("lightpink"), Color("lightyellow"),Color("lightgreen"), Color("lightblue"), Color("lightpurple")], center: .topLeading, startRadius: 845, endRadius: 111)
+            .background(RadialGradient(colors: [Color("lightpink"), Color("lightyellow"),Color("lightgreen"), Color("lightblue"), Color("lightpurple")], center: .topLeading, startRadius: 770, endRadius: 150))
                 .ignoresSafeArea()
             
             
