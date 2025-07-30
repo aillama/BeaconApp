8d77efc5798e3af066a413a0f6ac268d0c2667df
Ananya <ananya.sood270@gmail.com>
2025-07-30 12:39:54 -0500

    Worked on AngryPage and added all files to the project

---
+++ BeaconApp/TiredPage.swift
@@ -0,0 +1,18 @@
+//
+//  TiredPage.swift
+//  BeaconApp
+//
+//  Created by Arin Sood on 7/30/25.
+//
+
+import SwiftUI
+
+struct TiredPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TiredPage()
+}
